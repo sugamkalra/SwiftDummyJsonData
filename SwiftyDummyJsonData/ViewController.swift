01332e7e2ba5a398c2030ac215f1d74d2573164c
Sugam Kalra <sugamkalra@Sugams-MacBook-Pro.local>
2015-12-28 12:51:28 +0530

    Dummy JSON

--- SwiftyDummyJsonData/ViewController.swift
+++ SwiftyDummyJsonData/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.jsonParsingFromFile()
     }
-
+    
+    
+    func jsonParsingFromFile()
+    {
+        let path: NSString = NSBundle.mainBundle().pathForResource("helps", ofType: "json")!
+        
+        let data : NSData = try! NSData(contentsOfFile: path as String, options: NSDataReadingOptions.DataReadingMapped)
+        
+        self.startParsing(data)
+        
+    }
+    
+    func startParsing(data :NSData)
+    {
+        let dict: NSDictionary!=(try! NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers)) as! NSDictionary
+        
+        print(dict)
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
